4419c83a58675a195cdbb4c1784bfeee6b083caf
Logan Bennett <bennett.j.logan@gmail.com>
2025-02-11 17:44:24 -0800

    remove reminder to remain still

--- simple_span_rdoc_practice__fmri/experiment.js
+++ simple_span_rdoc_practice__fmri/experiment.js
@@ -805,7 +805,6 @@ var expID = 'simple_span_rdoc_practice__fmri';
 var endText = `
   <div class="centerbox" style="height: 50vh;">
     <p class="center-block-text">Thanks for completing this task!</p>
-     <p class="center-block-text">Please remain still for the remainder of the scan!</p>
   </div>`;
 
 var endBlock = {
